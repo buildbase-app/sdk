e2b6181489fff8006216f2d039b9f56ff0b7df0b
buildbase <support@buildbase.app>
2026-04-10 18:35:36 +0530

    v0.0.31

---
+++ src/css.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Type declaration for @buildbase/sdk/css
+ * This allows TypeScript to resolve the CSS side-effect import without errors.
+ */
+declare const styles: string;
+export default styles;
